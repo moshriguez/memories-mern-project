5570cbcfdb8c427a6c1a9607acb35f0a73c02067
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-08-06 15:27:07 -0700

    completed form and able to submit and persist data

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,9 +1,11 @@
+import { createPost } from "../api"
+
 export default (posts = [], action) => {
     switch (action.type) {
         case 'FETCH_ALL':
-            return posts
+            return action.payload
         case 'CREATE':
-            return posts
+            return [...posts, action.payload]
         default:
             return posts
     }
--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import postRoutes from "./routes/posts.js";
 
 const app = express()
 
-app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
+
+app.use('/posts', postRoutes)
 
 const CONNECTION_URL = 'mongodb+srv://moshriguez:N3kr0%24%21%244llpurp0%243%233r0@cluster0.xbly7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
